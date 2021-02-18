6a23f5d2afe3b9bb8950d297bf13198d450a97ae
rviard-vmw <79253947+rviard-vmw@users.noreply.github.com>
2021-02-18 12:18:20 +0100

    Create Hello.go

---
+++ Hello.go
@@ -0,0 +1,14 @@
+#line 3
+#line 4
+                # description:  my very first go program
+=======
+# add line 1
+# add line 2
+# description:  my very first go program
+                
+                package main
+                import "fmt"
+                func main() {
+                    fmt.Println("hello world")
+}
+ 
